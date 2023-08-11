d62e716fcfb3480a2b408fe13fee5f7f5e8e15c7
John Garrison <splendidonkey@gmail.com>
2023-08-11 13:54:49 -0500

    added fetch call to test front-end api access

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,9 +1,12 @@
 // script.js
 const apiRootUrl = 'https://api.edamam.com/api/recipes/v2?type=public&app_id=19533129&app_key=99f99420f7ca835f437b3ab67e21166e';
 
-function fetchRecipe(searchQuery) {
-  const apiSearchUrl = `${apiRootUrl}&${searchQuery}`;
+async function fetchRecipe(searchQuery) {
+  const apiSearchUrl = `${apiRootUrl}&q=${searchQuery}`;
   const searchResponse = await fetch(apiSearchUrl);
-  
-  console.log(searchResponse);
+
+  const searchData = await searchResponse.json();
+  console.log(searchData);
 }
+
+fetchRecipe("tacos");
