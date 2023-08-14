21c84e2c5914ebc417315d6ab1e1f281f4bf0728
Peter Lauer <lauerpeter11@gmail.com>
2023-08-14 12:39:22 -0500

    handle sign up function

--- public/assets/script.js
+++ public/assets/script.js
@@ -10,3 +10,32 @@ async function fetchRecipe(searchQuery) {
 }
 
 fetchRecipe("tacos");
+
+const users = []
+
+function handleSignUp(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Collect user information from the sign-up form
+  const emailInput = document.querySelector('input[name="email-name"]');
+  const passwordInput = document.querySelector('input[name="password"]');
+  const verifyPasswordInput = document.querySelector('input[name="verify-password"]');
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const verifyPassword = verifyPasswordInput.value;
+
+  // Check if passwords match
+  if (password !== verifyPassword) {
+      alert('Passwords do not match. Please try again.');
+      return;
+  }
+
+  console.log('User signed up:');
+  console.log('Email:', email);
+  console.log('Password:', password);
+
+  location.href = 'login.html'
+}
+
+document.getElementById('signUp').addEventListener('click', handleSignUp);
