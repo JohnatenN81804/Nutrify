3493cc0f9ba8c8218e6c07cca1d2c3b7d9269c87
Peter Lauer <lauerpeter11@gmail.com>
2023-08-15 14:20:09 -0500

    changed local storage login for requirements but not working yet

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -1,23 +1,59 @@
+
+
+const express = require('express');
 const bcrypt = require('bcrypt');
+const { User } = require('../models/define.js'); // Import the User model
+const router = express.Router();
 
-const authenticateUser = async (req, res, next) => {
+// User registration route
+router.post('/register', async (req, res) => {
   try {
-    // Session, user object attached to the request
-    if (!req.session || !req.session.user) {
-      return res.status(401).json({ message: 'Unauthorized' });
+    const { email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
     }
 
-    const { userName, password } = req.session.user;
-    const user = users[userName]; //Implement user object or database
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
+    // Create a new user
+    await User.create({ email, password_hash: hashedPassword });
 
-    next(); // User is authenticated, proceed to the protected route
+    return res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    res.status(500).json({ message: 'Error occurred' });
+    console.error(error);
+    return res.status(500).json({ message: 'Error occurred' });
   }
-};
+});
 
-module.exports = authenticateUser;
+// User login route
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    // Verify password
+    const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    // Set up a session or token as needed
+    // For example, create a session: req.session.user = { userId: user.id, email: user.email };
+
+    return res.status(200).json({ message: 'Authentication successful' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Error occurred' });
+  }
+});
+
+module.exports = router;
