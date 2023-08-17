912a02d399f6af3febe9fd57a11836255418624c
Peter Lauer <lauerpeter11@gmail.com>
2023-08-17 12:14:13 -0500

    login succesfully working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,14 +13,19 @@ router.post('/register', async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({
-      email: email,
-      password_hash: hashedPassword
+      email,
+      password: hashedPassword
     });
+
+
 
     if (!newUser) {
       res.status(400).json({ message: 'User already exists' });
     } else {
-      res.status(201).json({ message: 'User registered successfully' });
+      req.session.save(() => {
+        req.session.loggedIn = true;
+        res.status(201).json({ message: 'User registered successfully' });
+      });
     }
   } catch (error) {
     console.log(error)
@@ -31,13 +36,15 @@ router.post('/register', async (req, res) => {
 // Log in to the app with your personal information
 router.post('/login', async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
 
     const user = await User.findOne({
       where: {
-        username: username
+        email
       }
     });
+
+    console.log(user)
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ message: 'Invalid username or password' });
@@ -45,8 +52,10 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.loggedIn = true;
-      res.json({ message: 'Login successful' });
+      res.json({ message: 'Login successful' })
     });
+    console.log(req.session)
+;
 
   } catch (error) {
     res.status(500).json({ message: 'An error occurred' });
