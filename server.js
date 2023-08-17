84cfb390a81607f9877be196237bd296ed57d005
John Garrison <splendidonkey@gmail.com>
2023-08-17 14:54:04 -0500

    changed sync back to false

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
 
