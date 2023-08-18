2cff16174e66ee424ad8afaa85819434af60308a
John Garrison <splendidonkey@gmail.com>
2023-08-17 22:43:50 -0500

    fixed recipebox.html script tag and made new script for this page and added event listener for logout button to send DELETE request to server for logout/destroy session

--- public/assets/search.js
+++ public/assets/search.js
@@ -1,7 +1,7 @@
 const apiRootUrl = 'https://api.edamam.com/api/recipes/v2?type=public&app_id=19533129&app_key=99f99420f7ca835f437b3ab67e21166e';
 const searchBtn = $(`.search-btn`);
-const logoutBtn = $('#logout-btn')
-const resultCards = $('#result-cards')
+const logoutBtn = $('#logout-btn');
+const resultCards = $('#result-cards');
 
 async function fetchRecipe(searchQuery) {
   const apiSearchUrl = `${apiRootUrl}&q=${searchQuery}`;
